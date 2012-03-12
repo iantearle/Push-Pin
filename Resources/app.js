274eb99909e89bc893e7a95b7ce6de5650222e99
Ian Tearle <ian@iantearle.com>
2012-03-12 17:36:45 +0000

    Push pin for longpress

--- Resources/app.js
+++ Resources/app.js
@@ -1,189 +1,59 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+var win = Ti.UI.createWindow({
+	backgroundColor: '#fff',
+	fullscreen: false,
+	exitOnClose: true
+});
+var menu = require('/path').createMenu({
+	iconList: [
+		{ image: 'images/sm/share.png', id: 'Share' },
+		{ image: 'images/sm/permalink.png', id: 'Permalink' },
+		{ image: 'images/sm/expand.png', id: 'Expand' },
+		{ image: 'images/sm/search.png', id: 'Search' },
+		{ image: 'images/sm/rate.png', id: 'Rate' },
+		{ image: 'images/sm/post.png', id: 'Post' },
+		{ image: 'images/sm/location.png', id: 'Location' },
+		{ image: 'images/sm/flag.png', id: 'Flag' },
+		{ image: 'images/sm/privacy.png', id: 'Privacy' },
+		{ image: 'images/sm/rescue.png', id: 'Rescue' },
+		{ image: 'images/sm/refresh.png', id: 'Refresh' },
+		{ image: 'images/sm/favourite.png', id: 'Favourite' }
 
-Ti.include('Tween.js');
-TWEEN.start();
-
-var position1,position2,position3,position4,position5,position6;
-var tween;
-var rotate = 0;
-
-var li1 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16	
+	]
+});
+var label = Ti.UI.createLabel({
+	text: 'index: ???\nid: ???',
+	color: '#222',
+	font: {
+		fontSize: 24,
+		fontWeight: 'bold'	
+	},
+	textAlign: 'center'
 });
 
-var li2 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16
-	});
-var li3 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16
-});
-var li4 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16	
-});
-var li5 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16
-});
-var li6 = Titanium.UI.createImageView({
-	image: 'images/comment.png',
-	width: 32,
-	height: 32,
-	bottom: 16,
-	left: 16
+menu.addEventListener('iconClick', function(e) {
+	Ti.API.info(e.source);
+	Ti.API.info(e.index);
+	Ti.API.info(e.id);
+	label.text = 'index: ' + e.index + '\nid: ' + (e.id ? e.id : 'undefined');
 });
 
-var pushPin = Titanium.UI.createImageView({
-	image: 'images/cross.png',
-	width: 40,
-    height: 40,
-    bottom: 10,
-    left: 10
+var button = Ti.UI.createButton({
+	title: 'reset menu',
+	width: 120,
+	height: 40,
+	top: 20
+});
+button.addEventListener('click', function(e) {
+	menu.initMenu();
+	label.text = 'index: ???\nid: ???';
 });
 
-/* PushPin */
-var t = Ti.UI.create2DMatrix().rotate(-135);
-var r = Ti.UI.create2DMatrix().rotate(0);
-var spin = Titanium.UI.createAnimation();
-spin.transform = t;
-var respin = Titanium.UI.createAnimation();
-respin.transform = r;
-/* PushPin */
-
-var win = Ti.UI.createWindow({ backgroundColor: '#32394a' });
-
-win.add(li1);
-win.add(li2);
-win.add(li3);
-win.add(li4);
-win.add(li5);
-win.add(li6);
-win.add(pushPin);
-
-function animateLi1() {
-	li1.left = position1.x;
-	li1.bottom = position1.y;
-}
-function animateLi2() {
-	li2.left = position2.x;
-	li2.bottom = position2.y;
-}
-function animateLi3() {
-	li3.left = position3.x;
-	li3.bottom = position3.y;
-}
-function animateLi4() {
-	li4.left = position4.x;
-	li4.bottom = position4.y;
-}
-function animateLi5() {
-	li5.left = position5.x;
-	li5.bottom = position5.y;
-}
-function animateLi6() {
-	li6.left = position6.x;
-	li6.bottom = position6.y;
-}
-
-function openLi1() {
-	position1 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position1).to({x: 14, y: 150, rotation: 359}, 300).delay(20).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi1).start();
-}
-function openLi2() {
-	position2 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position2).to({x: 56, y: 143, rotation: 359}, 300).delay(30).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi2).start();
-}
-function openLi3() {
-	position3 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position3).to({x: 94, y: 124, rotation: 359}, 300).delay(40).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi3).start();
-}
-function openLi4() {
-	position4 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position4).to({x: 124, y: 94, rotation: 359}, 300).delay(50).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi4).start();
-}
-function openLi5() {
-	position5 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position5).to({x: 143, y: 56, rotation: 359}, 300).delay(60).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi5).start();
-}
-function openLi6() {
-	position6 = {x: 16, y: 16, rotation: 0};
-	tween = new TWEEN.Tween(position6).to({x: 150, y: 14, rotation: 359}, 300).delay(70).easing(TWEEN.Easing.Back.EaseOut).onUpdate(animateLi6).start();
-}
-
-
-function closeLi1() {
-	position1 = {x: 14, y: 150, rotation: 0};
-	tween = new TWEEN.Tween(position1).to({x: 16, y: 16, rotation: 359}, 300).delay(70).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi1).start();
-}
-function closeLi2() {
-	position2 = {x: 56, y: 143, rotation: 0};
-	tween = new TWEEN.Tween(position2).to({x: 16, y: 16, rotation: 359}, 300).delay(60).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi2).start();
-}
-function closeLi3() {
-	position3 = {x: 94, y: 124, rotation: 0};
-	tween = new TWEEN.Tween(position3).to({x: 16, y: 16, rotation: 359}, 300).delay(50).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi3).start();
-}
-function closeLi4() {
-	position4 = {x: 124, y: 94, rotation: 0};
-	tween = new TWEEN.Tween(position4).to({x: 16, y: 16, rotation: 359}, 300).delay(40).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi4).start();
-}
-function closeLi5() {
-	position5 = {x: 143, y: 56, rotation: 0};
-	tween = new TWEEN.Tween(position5).to({x: 16, y: 16, rotation: 359}, 300).delay(30).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi5).start();
-}
-function closeLi6() {
-	position6 = {x: 150, y: 14, rotation: 0};
-	tween = new TWEEN.Tween(position6).to({x: 16, y: 16, rotation: 359}, 300).delay(20).easing(TWEEN.Easing.Back.EaseIn).onUpdate(animateLi6).start();
-}
-
-pushPin.addEventListener('click',function(e){
-
-	if(rotate == 0) {
-		
-		pushPin.animate(spin);
-		
-		openLi1();
-		openLi2();
-		openLi3();
-		openLi4();
-		openLi5();
-		openLi6();
-
-		rotate = 135;
-		
-	} else {
-	
-		pushPin.animate(respin);
-		
-		closeLi1();
-		closeLi2();
-		closeLi3();
-		closeLi4();
-		closeLi5();
-		closeLi6();
-		
-		rotate = 0;
-		
-	}
-	
+win.addEventListener('longpress', function(e) {
+	Ti.App.fireEvent('animate_open');
 });
+
+win.add(menu);
+win.add(label);
+win.add(button);
+
 win.open();
---
+++ Resources/path.js
@@ -0,0 +1,351 @@
+// Path menu for Titanium
+// Tony Lukasavage - @tonylukasavage
+//
+// Notes:
+// - Transforms must be declared outside the animation to
+//   work on Android. (http://jira.appcelerator.org/browse/TIMOB-5796)
+
+// There MUST be more than 1 icon or the math breaks
+var DEFAULTS = {
+	ICON_IMAGE: '/images/star.png',
+	ICON_SIZE: Ti.Platform.displayCaps.platformWidth > 600 ? 80 : 64,
+	ICON_NUMBER: 12,
+	ICON_ROTATION: 0,
+	BUTTON_IMAGE: '/images/add.png',
+	BUTTON_SIZE: Ti.Platform.displayCaps.platformWidth > 600 ? 80 : 64,
+	MENU_DURATION: 300,
+	FADE_DURATION: 500,
+	BOUNCE_DISTANCE: 25,
+	STAGGER: 60
+};
+var isAndroid = Ti.Platform.osname === 'android';
+var isIOS = Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad';
+
+/////////////////////////////////////////
+////////// Module dependencies //////////
+/////////////////////////////////////////
+if (isIOS) {
+	var pathAnimator = require('path.animator');
+}
+
+/////////////////////////////////////////
+////////// "Private" variables //////////
+/////////////////////////////////////////
+var settings = {},
+	isAnimating = false,
+	menu, 
+    menuButton,
+    menuIcons,
+    fadeOut,
+    fadeIn;
+
+//////////////////////////////////////
+////////// "Public" members //////////
+//////////////////////////////////////
+exports.EVENT_ICONCLICK = 'iconClick';
+exports.createMenu = function(o) {
+	// Configure the settings for the menu
+	settings.iconList = o.iconList || createDefaultIconList();
+	settings.iconRotation = o.iconRotation || DEFAULTS.ICON_ROTATION;
+	settings.iconSize = o.iconSize || DEFAULTS.ICON_SIZE;
+	settings.buttonImage = o.buttonImage || DEFAULTS.BUTTON_IMAGE;
+	settings.buttonSize = o.buttonSize || DEFAULTS.BUTTON_SIZE;
+	settings.menuDuration = o.menuDuration || DEFAULTS.MENU_DURATION;
+	settings.fadeDuration = o.fadeDuration || DEFAULTS.FADE_DURATION;
+	settings.radius = o.radius || 260;
+	settings.bounceDistance = o.bounceDistance || DEFAULTS.BOUNCE_DISTANCE;
+	settings.stagger = o.stagger || DEFAULTS.STAGGER;
+	
+	// Create reusable fade & scale animations. Need to declare
+	// the transforms outside of the animation. See notes at the beginning
+	// of this file.
+	fadeOut = Ti.UI.createAnimation({
+		duration: settings.fadeDuration,
+		opacity: 0
+	});
+	fadeOut.transform = Ti.UI.create2DMatrix().scale(0, 0);
+	fadeLarge = Ti.UI.createAnimation({
+		duration: settings.fadeDuration,
+		opacity: 0 
+	});
+	fadeLarge.transform = Ti.UI.create2DMatrix().scale(4, 4);
+	
+	// Construct menu UI components and establish view hierarchy
+	menu = Ti.UI.createView({
+		opacity: 0
+	});
+	menuButton = createMenuButton();
+	menuIcons = [];
+	
+	Ti.App.addEventListener('animate_open', handleMenuButtonClick);
+	menuButton.addEventListener('click', handleMenuButtonClick);
+	
+	for (var i = 0; i < o.iconList.length; i++) {
+		var menuIcon = createMenuIcon(i);
+		menuIcon.addEventListener('click', handleMenuIconClick);
+		menuIcons.push(menuIcon);
+		menu.add(menuIcon);
+	}
+	menu.add(menuButton);
+	menu.initMenu = initMenu;
+	
+	return menu;
+};
+
+/////////////////////////////////////////
+////////// "Private" functions //////////
+/////////////////////////////////////////
+var resetIconVisibility = function(icon) {
+	// use a short timeout to prevent flicker
+	setTimeout(function() {
+		icon.opacity = 1;
+		icon.transform = Ti.UI.create2DMatrix().scale(1,1);
+		if (isAndroid) {
+			icon.show();
+		}
+	}, 100);
+};
+
+var initMenu = function() {
+	menuButton.isOpen = false;
+	menuButton.left = 0;
+	menuButton.bottom = 0;
+	menuButton.opacity = 1;
+	menuButton.transform = Ti.UI.create2DMatrix().rotate(0);
+	menuButton.show();
+	for (var i = 0; i < menuIcons.length; i++) {
+		var icon = menuIcons[i];
+		icon.left = 0;
+		icon.bottom = 0;
+		icon.right = 0;
+		icon.top = 0;
+		resetIconVisibility(icon);
+	}
+	isAnimating = false;
+};
+
+var handleMenuButtonClick = function(e) {
+	// Make sure we don't have other menu animations running
+	if (isAndroid && isAnimating === true) {
+		return;	
+	}
+	isAnimating = true;
+	
+	var i, icon;
+	var anim = menuButton.isOpen ? 'close' : 'open';
+	
+	// change the menu button state
+	menuButton.isOpen = !menuButton.isOpen;
+	menuButton.animate(menuButton.animations[anim]);
+	
+	// Open/close all the icons with animation
+	for (i = 0; i < menuIcons.length; i++) {
+		icon = menuIcons[i];
+		icon.animations[anim + 'Bounce'].addEventListener(
+			'complete', 
+			anim === 'open' ? doCompleteOpen : doCompleteClose,
+			anim === 'open' ? menu.opacity = 1 : menu.opacity = 1
+		);
+		icon.animate(icon.animations[anim + 'Bounce']);
+		
+		// ios uses the path.animator module for iOS rotations so that they can be
+		// greater than 180 degrees
+		if (isIOS) {
+			icon.rotate({
+				angle: settings.iconRotation,
+				duration: settings.menuDuration + (settings.menuDuration / 3.5)	
+			});
+		}
+	}
+};
+
+var handleMenuIconClick = function(e) {
+	var i, radians, icon;
+	
+	// Make sure we don't have other menu animations running
+	if (isAndroid && isAnimating === true) {
+		return;	
+	}
+	isAnimating = true;
+	
+	menu.fireEvent(exports.EVENT_ICONCLICK, {
+		source: menu,
+		icon: e.source,
+		index: e.source.index,
+		id: e.source.id
+	});
+	
+	// fade and scale out the menuButton
+	if (isAndroid) {
+		fadeOut.left = (menuButton.width * 0.5);
+		fadeOut.bottom = -1 * (menuButton.height * 0.5);		
+	}	
+	menuButton.animate(fadeOut);
+	
+	// iterate through icons, fade and scale down the ones that weren't clicked,
+	// fade and scale up the one that was.
+	for (i = 0; i < menuIcons.length; i++) {
+		radians = (360 / (menuIcons.length - 1)) * i * Math.PI / 180;
+		icon = menuIcons[i];
+		
+		// android scales from the top left, not the center like ios,
+		// hence the extra left/bottom animations
+		if (i !== e.source.index) {
+			if (isAndroid) {
+				fadeOut.left = Math.sin(radians) * settings.radius + (icon.width * 0.5);
+				fadeOut.bottom = Math.cos(radians) * settings.radius - (icon.height * 0.5);		
+			}	
+			icon.animate(fadeOut);
+		} else {
+			if (isAndroid) {
+				fadeLarge.left = Math.sin(radians) * settings.radius - (icon.width * 1.5);
+				fadeLarge.bottom = Math.cos(radians) * settings.radius + (icon.height * 1.5);
+			}
+			icon.animate(fadeLarge);
+		}	
+	}
+};
+
+var createMenuButton = function() {
+	var animations = {
+		open: Ti.UI.createAnimation({
+			duration: settings.menuDuration	
+		}),
+		close: Ti.UI.createAnimation({
+			duration: settings.menuDuration
+		})
+	};
+	animations.open.transform = Ti.UI.create2DMatrix().rotate(45);
+	animations.open.addEventListener('complete', function() {
+		isAnimating = false;
+	});
+	
+	// In Titanium, Android rotations always start at zero, regardless of last position.
+	// In Android Titanium apps you can pass two arguments to the rotate() function,
+	// the first being the starting rotation, the second being the final rotation.
+	// This is not a cross-platform method, so you need to make sure you are on Android
+	// before using 2 arguments.
+	// Jira Issue: http://jira.appcelerator.org/browse/TIMOB-6843
+	if (isAndroid) {
+		animations.close.transform = Ti.UI.create2DMatrix().rotate(45, 0);
+	} else {
+		animations.close.transform = Ti.UI.create2DMatrix().rotate(0);	
+	}
+	animations.close.addEventListener('complete', function() {
+		isAnimating = false;
+	});
+	
+	var menuButton = Ti.UI.createImageView({
+		image: settings.buttonImage,
+		height: settings.buttonSize,
+		width: settings.buttonSize,
+		left: 0,
+		bottom: 0,
+		right: 0,
+		top: 0,
+		isOpen: false,
+		animations: animations
+	});
+	
+	return menuButton;
+};
+
+var createMenuIcon = function(index) {
+	var length = settings.iconList.length;
+	var id = settings.iconList[index].id;
+	var radians = (360 / (length - 1)) * index * Math.PI / 196;
+	var bounceLeft = Math.sin(radians) * (settings.radius + settings.bounceDistance);
+	var bounceBottom = Math.cos(radians) * (settings.radius + settings.bounceDistance);
+	var finalLeft = Math.sin(radians) * settings.radius;
+	var finalBottom = Math.cos(radians) * settings.radius;
+	var animations = {
+		openBounce: Ti.UI.createAnimation({
+			duration: settings.menuDuration,
+			bottom: bounceBottom,
+			left: bounceLeft,
+			delay: index * settings.stagger
+		}),
+		openFinal: Ti.UI.createAnimation({
+			duration: settings.menuDuration / 3.5,
+			bottom: finalBottom,
+			left: finalLeft
+		}),
+		closeBounce: Ti.UI.createAnimation({
+			duration: settings.menuDuration / 3.5,
+			bottom: bounceBottom,
+			left: bounceLeft,
+			delay: (length - (index+1)) * settings.stagger,
+		}),
+		closeFinal: Ti.UI.createAnimation({
+			duration: settings.menuDuration,
+			bottom: 0,
+			left: 0,
+			top: 0,
+			right: 0
+		})
+	};
+	
+	// iOS uses path.animator module for rotations
+	if (!isIOS) {
+		animations.openBounce.transform = Ti.UI.create2DMatrix().rotate(settings.iconRotation);
+		animations.closeFinal.transform = Ti.UI.create2DMatrix().rotate(-1 * settings.iconRotation);
+	}
+	
+	// Use path.animator view for iOS, which can be rotated more than 180 degrees, 
+	// by default 720 degrees
+	var icon;
+	if (isIOS) {
+		icon = pathAnimator.createView({
+			backgroundImage: settings.iconList[index].image,
+			height: settings.iconSize,
+			width: settings.iconSize,
+			left: 0,
+			bottom: 0,
+			right: 0,
+			top: 0,
+			animations: animations,
+			index: index,
+			id: id
+		});
+	} else {
+		icon = Ti.UI.createImageView({
+			image: settings.iconList[index].image,
+			height: settings.iconSize,
+			width: settings.iconSize,
+			left: 0,
+			bottom: 0,
+			right: 0,
+			top: 0,
+			animations: animations,
+			index: index,
+			id: id
+		});
+	}
+	
+	icon.animations.openBounce.icon = icon;
+	icon.animations.closeBounce.icon = icon;
+	
+	return icon;
+};
+
+var doCompleteOpen = function(e) {
+	e.source.removeEventListener('complete', doCompleteOpen);
+	e.source.icon.animate(e.source.icon.animations.openFinal);
+};
+
+var doCompleteClose = function(e) {
+	e.source.removeEventListener('complete', doCompleteClose);
+	e.source.icon.animate(e.source.icon.animations.closeFinal);
+	setTimeout(function(){menu.opacity = 0},1000)
+};
+
+var createDefaultIconList = function() {
+	var icons = [];
+	for (var i = 0; i < DEFAULTS.ICON_NUMBER; i++) {
+		icons.push({
+			image: DEFAULTS.ICON_IMAGE,
+			id: undefined
+		});	
+	}
+	return icons;	
+};
